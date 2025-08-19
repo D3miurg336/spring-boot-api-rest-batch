242ba0a7ad7648bf7f2494d10ba41217422fe0b5
dinot <dinot@admin>
2025-08-19 14:20:02 -0600

    se creo un nuevo recurso para mandar el mensaje de la reunion de prod

---
+++ spring-boot-ai-deepseek/src/main/java/org/ai/nuevo/recurso/NuevoRecurso.java
@@ -0,0 +1,11 @@
+package org.ai.nuevo.recurso;
+
+public class NuevoRecurso {
+	public static void main(String[] args) {
+		try {
+			System.out.println("454");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
